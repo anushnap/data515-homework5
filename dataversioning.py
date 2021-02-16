467ced169c1135d7a163121a823e8135794d7025
anushnap <anushnap@uw.edu>
2021-02-15 19:35:30 -0800

    initial commit

---
+++ dataversioning.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+class DataHistory:
+    
+    def __init__(self, data):
+        pass
+
+    def add(self, commit):
+        pass
+
+    def checkout(self, name):
+        pass
+
+    
+class DataCommit:
+
+    def __init__(self, name):
+        pass
